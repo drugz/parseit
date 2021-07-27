7c5a4c0b47c1c4c24a7be75d163cbca0022fad7c
Oleg Korzhanov <ok@xfactorial.com>
2021-07-27 15:27:10 +0300

    fix emulateMediaType

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Hi");
+const puppeteer = require('puppeteer');
+
+(async() => {
+  const browser = await puppeteer.launch() ; 
+  const page = await browser.newPage();
+
+  await page.goto('http://devconf.ru/') ; 
+  await page.emulateMediaType('screen') ; 
+  await page.pdf({
+    path: './devconf.pdf', 
+    printBackground: true 
+  });
+await browser.close() ;
+})();
