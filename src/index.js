356cefde11a367097f628e82914684480519086b
Oleg Korzhanov <ok@xfactorial.com>
2021-07-27 15:58:26 +0300

    load pdf and make screenshot

--- src/index.js
+++ src/index.js
@@ -2,21 +2,28 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     var fs = require('fs');
-    var dir = './result';
+    var resultDir = './result';
 
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
+    if (!fs.existsSync(resultDir)) {
+        fs.mkdirSync(resultDir);
     }
+    var dir = resultDir + "/";
+
 
     const browser = await puppeteer.launch({
-        headless: false
-      });
+        // headless: false
+    });
     const page = await browser.newPage();
 
     await page.goto('http://devconf.ru/');
     await page.emulateMediaType('screen');
     await page.pdf({
-        path: dir+'/devconf.pdf',
+        path: dir + 'devconf.pdf',
+        printBackground: true
+    });
+
+    await page.screenshot({
+        path: dir + 'screenshot.png',
         printBackground: true
     });
     await browser.close();
